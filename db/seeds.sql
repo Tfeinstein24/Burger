ab88dccf5b4fcb3d3167ff576055163af414e0e3
Taylor Feinstein <Taylor Feinstein>
2017-06-11 16:55:32 -0500

    file structure set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date_eaten) VALUES ('Squirrel and Goat Cheese Burger', true, '2017-06-11 00:00:01');
+INSERT INTO burgers (burger_name, devoured, date_eaten) VALUES ('Chipmunk and Brie Burger', true, '2017-06-10 00:00:01');
+INSERT INTO burgers (burger_name, devoured, date_eaten) VALUES ('Gopher and Swiss Burger', true, '2017-06-09 00:00:01');
