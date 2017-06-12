1285ea662ad56d5e44d06f528733aa846b23761e
Taylor Feinstein <Taylor Feinstein>
2017-06-12 11:09:02 -0500

    ports working now

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 8080;
+var PORT = process.env.PORT || 8888;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+//app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,4 +25,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
